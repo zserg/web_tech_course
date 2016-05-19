bc12577243691cf4873ba9b4d3135c7b1a705604
zserg <zsergt3@gmail.com>
2016-05-19 14:02:06 +0000

    ...

--- init.sh
+++ init.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+git config --global user.email "zsergt3@gmail.com"    
+git config --global user.name "zserg"              
 
 sudo ln -s /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/test.conf
 sudo rm  /etc/nginx/sites-enabled/default
