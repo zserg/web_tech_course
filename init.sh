d390aec522a77fbb8bc229a55c12715066f9e938
zserg <zsergt3@gmail.com>
2016-05-19 14:01:10 +0000

    gunicorn added

--- init.sh
+++ init.sh
@@ -4,3 +4,6 @@ sudo ln -s /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/test.conf
 sudo rm  /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
 
+sudo ln -s /home/box/web/etc/gunicorn.conf   /etc/gunicorn.d/test
+sudo /etc/init.d/gunicorn restart
+
