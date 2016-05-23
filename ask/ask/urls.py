e391608adb0ef7ca1602abb2a624c3e52f434536
ZSerg <zsergt3@gmail.com>
2016-05-23 23:29:08 +0300

    ...

--- ask/ask/urls.py
+++ ask/ask/urls.py
@@ -11,6 +11,8 @@ urlpatterns = patterns('',
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^$', views.question_list, name='question-list'),
+    url(r'^popular/$', views.popular_list, name='popular_list'),
     url(r'^question/(\d+)/', views.question_details, name='question-details'),
 
 )
--- ask/qa/views.py
+++ ask/qa/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, get_object_or_404
 from django.views.decorators.http import require_GET
 from django.http import HttpResponse
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 
 from .models import Question, Answer
 
@@ -14,6 +15,45 @@ def question_details(request, slug):
 
     return render(request, 'qa/question_details.html',
                   {'question' : question,
-                   'answers' : answers})
+                   'answers' : answers}
+                  )
+
+
+@require_GET
+def question_list(request):
+    question_list = Question.objects.all().order_by('-added_at')
+    paginator = Paginator(question_list, 10)
+
+    page = request.GET.get('page')
+    try:
+       questions = paginator.page(page)
+    except PageNotAnInteger:
+       questions = paginator.page(1)
+    except EmptyPage:
+       questions = paginator.page(paginator.num_pages)
+
+    return render(request, 'qa/question_list.html',
+                  {'questions' : questions}
+                  )
+
+
+@require_GET
+def popular_list(request):
+    popular_list = Question.objects.all().order_by('-rating')
+    paginator = Paginator(popular_list, 10)
+
+    page = request.GET.get('page')
+    try:
+       pquestions = paginator.page(page)
+    except PageNotAnInteger:
+       pquestions = paginator.page(1)
+    except EmptyPage:
+       pquestions = paginator.page(paginator.num_pages)
+
+    return render(request, 'qa/popular_list.html',
+                  {'pquestions' : pquestions}
+                  )
+
+
 
 
